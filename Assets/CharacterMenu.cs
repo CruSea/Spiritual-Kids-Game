9cf47bc9e86831d1a5fe9c9a9710f758855c18d4
diye <diyye101@gmail.com>
2019-10-05 12:25:15 +0300

    first commit

---
+++ Assets/CharacterMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CharacterMenu : MonoBehaviour
+{
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    public void PlayAsDavid() 
+    {
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("David'sMainMenu");
+
+    }
+}
